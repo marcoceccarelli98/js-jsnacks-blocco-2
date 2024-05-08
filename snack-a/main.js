071d454c6bb45e71aa67ca45c98b7424bb542596
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-08 15:25:30 +0200

    [Snack a] Changed function name

--- snack-a/main.js
+++ snack-a/main.js
@@ -2,9 +2,9 @@
 
 const arrayTest = [1, 2, 3, 4, 5, 6, 7, 8, 'ciao', 10];
 
-console.log(arrayLength(arrayTest));
+console.log(contaElementi(arrayTest));
 
-function arrayLength(array) {
+function contaElementi(array) {
     let count = 0;
 
     while (array[count] != undefined) {
--- snack-b/main.js
+++ snack-b/main.js
@@ -1,2 +1,21 @@
 'use strict';
 
+
+
+function trovaIndice(array, element) {
+
+    for () {
+
+    }
+
+}
+
+function contaElementi(array) {
+    let count = 0;
+
+    while (array[count] != undefined) {
+        count++;
+    }
+
+    return count;
+}
