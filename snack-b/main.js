e8d6afff1087adb1969b6f5f215a0d619dfe6dee
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-08 15:34:13 +0200

    [Snack b] Added script

--- snack-b/main.js
+++ snack-b/main.js
@@ -1,13 +1,24 @@
 'use strict';
 
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 'ciao', 10];
 
+const elementToFind = prompt('Inserisci l\'elemento da cercare');
+
+console.log(trovaIndice(testArray, elementToFind));
+
+
+// ---------
+// FUNCTIONS
+// ---------
 
 function trovaIndice(array, element) {
 
-    for () {
-
+    for (let i = 0; i < contaElementi(array); i++) {
+        if (array[i] == element) {
+            return i;
+        }
     }
-
+    return -1;
 }
 
 function contaElementi(array) {
